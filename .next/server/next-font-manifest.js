404f98d6851fa2d7f2a18f6da422ec8c2a812e7d
Marcus Dutra <navalservice1@gmail.com>
2025-01-19 00:01:43 -0300

    first commit

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/marcus/GitHub/Mainnet Design/mainnet-ui/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\",\"static/media/c39eadd14305e40a-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
---
+++ components/header.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import dynamic from 'next/dynamic';
+import * as FancyButton from '@/components/ui/fancy-button';
+import { Button } from './ui/file-upload';
+
+const DynamicThemeSwitch = dynamic(() => import('./theme-switch'), {
+  ssr: false,
+});
+
+export default function Header() {
+  
+  return (
+    <>
+      <header className='w-full flex h-14 max-w-5xl items-center justify-between'>
+        <Link
+          href='/'
+          className='flex items-center gap-2 text-title-h6 text-text-strong-950'
+        >
+          <img
+            src='/images/logo.svg'
+            alt=''
+            className='size-16 object-contain'
+          />
+          <div className="text-title-h4 text-text-strong-950">Mainnet</div>
+        </Link>
+
+    <FancyButton.Root variant='neutral'>Get Started</FancyButton.Root>
+
+
+      </header>
+    </>
+  );
+}
