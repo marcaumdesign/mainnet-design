8e6b05979028981eeb127d88eab4846dfe216fe6
Marcus Dutra <navalservice1@gmail.com>
2025-01-19 21:26:03 -0300

    corrections

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "13354aab045918e7"; }
+/******/ 		__webpack_require__.h = function() { return "2b422c3bf18ad2d6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
