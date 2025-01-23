ea034ff359e952c3ee2f0ec7ee680b0e4c3bb748
Marcus Dutra <navalservice1@gmail.com>
2025-01-23 20:26:14 -0300

    changes

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -125,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("597580868ad82e6c")
+/******/ 		__webpack_require__.h = () => ("1b078889b3966a67")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "996a80c24f5bdc55"; }
+/******/ 		__webpack_require__.h = function() { return "3ca91d932253d702"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
