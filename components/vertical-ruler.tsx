5bbf879d1664491121b9848686cb537e84845992
Marcus Dutra <navalservice1@gmail.com>
2025-01-23 18:58:24 -0300

    vertical ruler mouse effect added

--- components/vertical-ruler.tsx
+++ components/vertical-ruler.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react'
 
 export default function VerticalRuler() {
   const [height, setHeight] = useState(0)
+  const [mouseY, setMouseY] = useState(0)
 
   useEffect(() => {
     const updateHeight = () => {
@@ -13,13 +14,19 @@ export default function VerticalRuler() {
       ))
     }
 
+    const handleMouseMove = (event: MouseEvent) => {
+      setMouseY(event.clientY)
+    }
+
     updateHeight()
     window.addEventListener('resize', updateHeight)
     window.addEventListener('load', updateHeight)
+    window.addEventListener('mousemove', handleMouseMove)
 
     return () => {
       window.removeEventListener('resize', updateHeight)
       window.removeEventListener('load', updateHeight)
+      window.removeEventListener('mousemove', handleMouseMove)
     }
   }, [])
 
@@ -29,20 +36,31 @@ export default function VerticalRuler() {
         className="relative w-full"
         style={{ height: `${height}px` }}
       >
-        {[...Array(Math.ceil(height / 50))].map((_, i) => (
-          <div 
-            key={i} 
-            className="absolute left-0 flex items-center"
-            style={{ top: `${i * 50}px` }}
-          >
-            <span className="text-subheading-2xs text-text-soft-400 w-8 text-right pr-1">
-              {i * 50}
-            </span>
-            <div className="w-3 border-t border-gray-300" />
-          </div>
-        ))}
+        {[...Array(Math.ceil(height / 50))].map((_, i) => {
+          const isCurrent = mouseY >= i * 50 && mouseY < (i + 1) * 50
+          const isPrevious = mouseY >= (i - 1) * 50 && mouseY < i * 50
+          const isNext = mouseY >= (i + 1) * 50 && mouseY < (i + 2) * 50
+
+          return (
+            <div 
+              key={i} 
+              className="absolute left-0 flex items-center"
+              style={{ top: `${i * 50}px` }}
+            >
+              <span 
+                className={`text-subheading-2xs w-8 text-right pr-1 ${
+                  isCurrent ? 'text-text-strong-950' : 
+                  isPrevious || isNext ? 'text-text-sub-600' : 
+                  'text-text-disabled-300'
+                }`}
+              >
+                {i * 50}
+              </span>
+              <div className="w-3 border-t border-gray-300" />
+            </div>
+          )
+        })}
       </div>
     </div>
   )
 }
-
