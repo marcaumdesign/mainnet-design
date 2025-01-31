3a4ce88a4b8b8b4b660419dec6b5aaab496e6a00
Marcus Dutra <navalservice1@gmail.com>
2025-01-31 16:10:04 -0300

    📦 NEW: portoflio page created, closes #35

---
+++ app/portfolio/page.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import { useRouter, useParams } from 'next/navigation';
+import { PortfolioProps, portfolio } from '@/data/portfolio';
+import Header from '@/components/header';
+import {
+  RiArrowLeftSLine,
+} from '@remixicon/react';
+import Footer from '@/components/footer';
+import VerticalRuler from '@/components/vertical-ruler';
+import PortfolioCard from '@/components/portfolio-card';
+
+
+const Page = () => {
+
+
+    const items: PortfolioProps[] = portfolio;
+  const router = useRouter();
+
+  return (
+    <div className='relative flex w-full flex-col items-center'>
+      <Header size='full' />
+      <div className='relative inline-flex h-fit w-full max-w-[1100px] flex-col items-start justify-start border-l border-r border-stroke-soft-200 bg-bg-white-0'>
+        <VerticalRuler className='absolute left-[-55px]' />
+        <div className='flex h-fit flex-col items-start justify-center gap-4 self-stretch border-t border-stroke-soft-200 p-8 pt-[122px]'>
+        <div className='inline-flex items-center justify-center gap-1'>
+            <div
+              className='cursor-pointer text-subheading-sm uppercase text-text-soft-400 hover:text-text-strong-950'
+              onClick={() => router.push('/')}
+            >
+              Home
+            </div>
+            <RiArrowLeftSLine />
+            <div
+              className='text-subheading-sm uppercase text-text-soft-400 hover:text-text-strong-950'
+              onClick={() => router.push('/portoflio')}
+            >
+              Portoflio
+            </div>
+            <RiArrowLeftSLine />
+          </div>
+          <div className="flex align-top"><div className='text-title-h0 text-text-strong-950'>Works</div><div className="text-title-h5">[{items.length}]</div></div>
+        </div>
+
+        <div className='flex-col md:grid md:grid-cols-2 border-t border-stroke-soft-200'>
+            {items.map((item) => (
+                <PortfolioCard key={item.name} {...item} />
+            ))}
+        </div>
+
+       
+      </div>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Page;
