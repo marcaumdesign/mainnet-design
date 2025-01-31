107f2258249597f492fdcee6f6cc28ca7013e089
Marcus Dutra <navalservice1@gmail.com>
2025-01-31 15:18:13 -0300

    📦 NEW: more projects section created, closes #34

--- components/portoflio-ticker.tsx
+++ components/portoflio-ticker.tsx
@@ -11,22 +11,25 @@ console.log(portfolio)
 const firstRow = portfolio.slice(0, portfolio.length / 2);
 const secondRow = portfolio.slice(portfolio.length / 2);
 
+interface PortfolioTickerProps {
+  hideSecondMarquee?: boolean;
+}
 
-export function PortfolioTicker() {
+export function PortfolioTicker({ hideSecondMarquee = false }: PortfolioTickerProps) {
   return (
     <div className="z-50 bg-bg-white-0 relative flex h-fit lg:h-fit w-full flex-col items-center justify-center overflow-hidden bg-background md:shadow-xl">
       <Marquee pauseOnHover className="[--duration:20s]">
         {firstRow.map((portfolio) => (
           <PortfolioCard key={portfolio.name} {...portfolio}/>
-          
         ))}
       </Marquee>
-      <Marquee reverse pauseOnHover className="[--duration:20s]">
-        {secondRow.map((portfolio) => (
-          <PortfolioCard key={portfolio.name} {...portfolio}/>
-        ))}
-      </Marquee>
-      
+      {!hideSecondMarquee && (
+        <Marquee reverse pauseOnHover className="[--duration:20s]">
+          {secondRow.map((portfolio) => (
+            <PortfolioCard key={portfolio.name} {...portfolio}/>
+          ))}
+        </Marquee>
+      )}
     </div>
   );
 }
