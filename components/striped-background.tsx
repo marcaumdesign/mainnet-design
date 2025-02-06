0288ae621369cdb0ac13ba4143e8eb62c38fd9a9
Thiago Arruda <thiago_arruda@id.uff.br>
2025-02-06 13:36:50 -0300

    🐛 FIX: Criar um modo escuro para o componente StrippedBackground

--- components/striped-background.tsx
+++ components/striped-background.tsx
@@ -1,61 +1,61 @@
-import { useEffect, useState } from "react"
+'use client';
+import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
+
+type Theme = 'light' | 'dark' | undefined;
+
+type ColorTheme = {
+  stripeColor: string;
+  backgroundColor: string;
+};
 
 interface StripedBackgroundProps {
-    className?: string
-    variant?: "light" | "dark"
-    stripeColor?: string
-    backgroundColor?: string
-    stripeWidth?: number
-    stripeAngle?: number
-  }
-  
-  export function StripedBackground({
-    className = "",
-    stripeColor, // Tailwind gray-100
-    backgroundColor,
-    stripeWidth = 3,
-    stripeAngle = 45,
-    variant = "light",
-  }: StripedBackgroundProps) {
-    // Calculate the stripe size to ensure proper repeating
+  className?: string;
+  stripeWidth?: number;
+  stripeAngle?: number;
+}
 
-      const [isDarkMode, setDarkMode] = useState<null | boolean>(null)
-        
-      
-        useEffect(() => {
-          
-          if(typeof window === 'undefined') {
-            return
-          }
-      
-      
-          const mode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-      
-          setDarkMode(mode)
-          
-        }, [])
+export function StripedBackground({
+  className = '',
+  stripeWidth = 3,
+  stripeAngle = 45,
+}: StripedBackgroundProps) {
+  const { systemTheme } = useTheme();
+  const [resolvedTheme, setResolvedTheme] = useState(systemTheme);
 
-    const size = stripeWidth * 2
+  useEffect(() => {
+    setResolvedTheme(systemTheme);
+  }, [systemTheme]);
 
-    { isDarkMode ? (stripeColor  = "#EBEBEB", backgroundColor  = "white") : (stripeColor = "#333", backgroundColor = "#181818")}
-    
-  
-    return (
-      <div
-        className={`w-full h-full ${className}`}
-        style={{
-          background: `linear-gradient(${stripeAngle}deg, 
-            ${stripeColor} 25%, 
-            ${backgroundColor} 25%, 
-            ${backgroundColor} 50%, 
-            ${stripeColor} 50%, 
-            ${stripeColor} 75%, 
-            ${backgroundColor} 75%, 
-            ${backgroundColor} 100%)`,
-          backgroundSize: `${size}px ${size}px`,
-        }}
-      />
-    )
-  }
-  
-  
+  const size = stripeWidth * 2;
+  const isLightTheme = resolvedTheme === 'light';
+
+  const colors: Record<NonNullable<Theme>, ColorTheme> = {
+    light: {
+      stripeColor: '#EBEBEB',
+      backgroundColor: '#FFFFFF',
+    },
+    dark: {
+      stripeColor: '#333333',
+      backgroundColor: '#181818',
+    },
+  };
+
+  return (
+    <div
+      key={resolvedTheme}
+      className={`h-full w-full ${className}`}
+      style={{
+        background: `linear-gradient(${stripeAngle}deg, 
+          ${colors[resolvedTheme!]?.stripeColor} 25%, 
+          ${colors[resolvedTheme!]?.backgroundColor} 25%, 
+          ${colors[resolvedTheme!]?.backgroundColor} 50%, 
+          ${colors[resolvedTheme!]?.stripeColor} 50%, 
+          ${colors[resolvedTheme!]?.stripeColor} 75%, 
+          ${colors[resolvedTheme!]?.backgroundColor} 75%, 
+          ${colors[resolvedTheme!]?.backgroundColor} 100%)`,
+        backgroundSize: `${size}px ${size}px`,
+      }}
+    />
+  );
+}
