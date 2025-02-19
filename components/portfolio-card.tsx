02db77f5eb896f4b1a7b3d1f415693df270be7dc
marcaumdesign <navalservice1@gmail.com>
2025-02-18 22:16:24 -0500

    👌 IMPROVE: Atualizado importação do botão e ajustado layout do cartão do portfólio para melhor responsividade

--- components/portfolio-card.tsx
+++ components/portfolio-card.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import * as Tag from '@/components/ui/tag';
-import * as Button from '@/components/ui/button-shadcn';
+import * as Button from '@/components/ui/button';
 
 import {
   RiSmartphoneFill,
@@ -63,19 +63,19 @@ const PortfolioCard: React.FC<PortfolioProps> = ({
   return (
     <Link
         href={`/portfolio/${slug}`}
-        className='flex items-center text-title-h6 text-text-strong-950 border border-stroke-soft-200 hover:border-stroke-sub-300'
+        className='flex items-center text-title-h6 text-text-strong-950'
       >
-    <div className='bg-bg-white-0 flex flex-col w-full '>
+    <div className='bg-bg-white-0 flex flex-col w-full max-w-xs lg:max-w-none'>
       <img
       className='w-full h-auto object-cover'
       src={imageUrl}
       alt={name}
       />
-      <div className='flex items-center justify-between px-4 py-4'>
+      <div className='flex items-center justify-between border border-stroke-soft-200 px-4 py-4'>
       <div className='text-label-md text-text-strong-950'>{name}</div>
       <div className='flex items-center gap-2'>
         <Tag.Root variant='stroke'>
-        <Tag.Icon as={icons[variant]}/>
+        <Tag.Icon as={icons[variant]} />
         {capitalizer(variant)}
         </Tag.Root>
       </div>
