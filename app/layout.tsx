c3ba357f722d83aeabff18b94cecf84326f8e5dd
marcaumdesign <navalservice1@gmail.com>
2025-04-03 12:47:36 -0300

    🔧 REFINAR: Ajusta a formatação do código do Meta Pixel no layout, mantendo a consistência na utilização de aspas

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,8 +61,8 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1177142284032271');
-fbq('track', 'PageView');`}
+fbq("init", "1177142284032271");
+fbq("track", "PageView");`}
         </script>
         <noscript>
           <img
