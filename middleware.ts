2459b732c1e4373d077737db374a3642bcbecf37
marcaumdesign <navalservice1@gmail.com>
2025-06-10 18:16:47 -0300

    🔧 ATUALIZAR: Remove código comentado do middleware e atualiza a versão do Next.js para 15.3.3, garantindo compatibilidade e melhorias de desempenho

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,8 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  const country = request.geo?.country || 'US';
-  const response = NextResponse.next();
-  response.cookies.set('user-country', country);
-  return response;
+  // // const country = request.geo?.country || 'US';
+  // // const response = NextResponse.next();
+  // // response.cookies.set('user-country', country);
+  // return response;
 } 
