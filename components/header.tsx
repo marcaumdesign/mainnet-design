a9c1d64a625f9aea67483aee73098870e3e3f7a3
marcaumdesign <navalservice1@gmail.com>
2025-06-16 11:05:25 -0300

    🔧 REFINAR: Atualiza a importação do botão e ajusta a formatação do cabeçalho, melhorando a clareza do código e a responsividade dos componentes

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import dynamic from 'next/dynamic';
 import * as FancyButton from '@/components/ui/fancy-button';
-import * as Button from './ui/button-shadcn';
+import * as Button from './ui/button';
 import { RainbowButton } from '@/components/ui/rainbow-button';
 import Logo from './logo';
 import { portfolio, PortfolioProps } from '@/data/portfolio';
@@ -13,19 +13,18 @@ const DynamicThemeSwitch = dynamic(() => import('./theme-switch'), {
 interface HeaderProps {
   size?: 'compact' | 'full';
   portfolio?: PortfolioProps[];
-  slug?: string,
+  slug?: string;
 }
 
 export default function Header({ size, portfolio = [], slug }: HeaderProps) {
   const item = portfolio.find((item) => item.slug === slug);
 
-  const maxWidthClass =
-    size === 'full' ? 'max-w-[1100px] ' : 'max-w-[600px]';
+  const maxWidthClass = size === 'full' ? 'max-w-[1100px] ' : 'max-w-[600px]';
 
   return (
     <>
       <header
-        className={`fixed z-10 flex h-fit w-full border-b md:border-l md:border-r border-stroke-soft-200 bg-bg-white-0 px-3 py-3 lg:px-8 lg:py-4 ${maxWidthClass} items-center justify-between shadow-[0px_16px_32px_-12px_rgba(14,18,27,0.10)]`}
+        className={`fixed z-10 flex h-fit w-full border-b border-stroke-soft-200 bg-bg-white-0 px-3 py-3 md:border-l md:border-r lg:px-8 lg:py-4 ${maxWidthClass} items-center justify-between shadow-[0px_16px_32px_-12px_rgba(14,18,27,0.10)]`}
       >
         <Link
           href='/'
@@ -34,16 +33,20 @@ export default function Header({ size, portfolio = [], slug }: HeaderProps) {
           <Logo className='size-16 object-contain' />
         </Link>
 
-        
+        <div className='flex items-center gap-2'>
+          <Button.Root mode='stroke' variant='neutral' className='h-[42px] text-label-md w-fit px-2 md:px-2'>
+            View Work
+          </Button.Root>
+
           <RainbowButton
             onClick={() =>
               window.open('https://calendly.com/marcaum/design', '_blank')
             }
-            className='w-fit px-4 md:px-8'
+            className=' w-fit px-2 md:px-2'
           >
             Get Started
           </RainbowButton>
-  
+        </div>
       </header>
     </>
   );
